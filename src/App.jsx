2d1ca8a35839a678fb24158af235d56ab98b3426
teja <tejasamudrala@2002.com>
2025-11-11 20:50:47 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import Home from "./navcomponents/Home";
 import Cart from "./navcomponents/Cart";
 import About from "./navcomponents/About";
+import { Routes } from "react-router-dom";
 function App() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-center mt-4">React Mart</h1>
       <React/>
       <search/>
-      <div className="navbar">
+      
         <Home/>
         <Cart/>
         <About/>
         <Cart/>
+      <div className="navbar">
+        <link  href="/Home">Home </link>     
+        <link  href="/Cart">Cart </link>
+        <link  href="About/">About</link>
+
+
+      </div>
+      <div>
+        <Routes path="" element=""></Routes>
       </div>
 
       <h1>hello</h1>
