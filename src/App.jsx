6b0c71926a4b8afb866ff7e4c0e87b090952edac
teja <tejasamudrala@2002.com>
2025-11-11 20:37:59 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Search from "./navcomponents/Search";
+import Home from "./navcomponents/Home";
+import Cart from "./navcomponents/Cart";
+import About from "./navcomponents/About";
 function App() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-center mt-4">React Mart</h1>
+      <React/>
       <search/>
+      <div className="navbar">
+        <Home/>
+        <Cart/>
+        <About/>
+        <Cart/>
+      </div>
+
       <h1>hello</h1>
     </div>
   );
