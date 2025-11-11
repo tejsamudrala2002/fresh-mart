7d2181d535386da21627c9f518753ded289b02b7
teja <tejasamudrala@2002.com>
2025-11-11 20:30:02 +0530

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Search from "./navcomponents/Search";
-
+import Search from "./navcomponents/Search";
 import
 function App() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-center mt-4">React Mart</h1>
-      
+      <search/>
       
     </div>
   );
